dad773a3157c69ffccc78a37c246e59ebaf7c673
Tim Austin <tim@neenjaw.com>
2019-12-04 15:46:46 -0600

    bad solution, too exclusive

--- day_04/lib/day04.ex
+++ day_04/lib/day04.ex
@@ -3,16 +3,56 @@ defmodule Day04 do
   Documentation for Day04.
   """
 
-  @doc """
-  Hello world.
+  @left 272091
+  @right 815432
 
-  ## Examples
+  def problem() do
+    @left..@right
+    |>Enum.filter(&check_one/1)
+    |>Enum.count()
+  end
 
-      iex> Day04.hello()
-      :world
+  def problem2() do
+    @left..@right
+    |>Enum.filter(&check_two/1)
+    |>Enum.count()
+  end
 
-  """
-  def hello do
-    :world
+  def check_one(number), do: number |> Integer.digits() |> is_valid()
+
+  def check_two(number), do: number |> Integer.digits() |> (fn nl -> is_valid(nl) and is_still_valid(nl) end).()
+
+  def is_valid([_,_,_,_,_,_] = password) do
+    chunked = password |> Enum.chunk_every(2,1,:discard)
+    increasing = chunked |> Enum.all?(fn [a,b] -> a <= b end)
+    double = chunked |> Enum.any?(fn [a,b] -> a == b end)
+
+    increasing and double
+  end
+
+  def is_valid(_), do: false
+
+  def is_still_valid(password) do
+    doubles =
+      password
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.filter(fn
+        [a,a] -> true
+        [_,_] -> false
+      end)
+      |> Enum.map(fn [a,_] -> a end)
+
+    max_double =
+      case doubles do
+        [] -> -1
+        l -> Enum.max(l)
+      end
+
+    maxes =
+      doubles
+      |> Enum.filter(fn n -> n == max_double end)
+      |> Enum.count()
+
+    maxes == 1
   end
 end
--- day_04/test/day04_test.exs
+++ day_04/test/day04_test.exs
@@ -2,7 +2,54 @@ defmodule Day04Test do
   use ExUnit.Case
   doctest Day04
 
-  test "greets the world" do
-    assert Day04.hello() == :world
+  test "v1" do
+    password = 111111
+
+    assert Day04.check_one(password)
+    refute Day04.check_two(password)
   end
+
+  test "v2" do
+    password = 223450
+
+    refute Day04.check_one(password)
+    refute Day04.check_two(password)
+  end
+
+  test "v3" do
+    password = 123789
+
+    refute Day04.check_one(password)
+    refute Day04.check_two(password)
+  end
+
+  test "v4" do
+    password = 112233
+
+    assert Day04.check_one(password)
+    assert Day04.check_two(password)
+  end
+
+  test "v5" do
+    password = 123444
+
+    assert Day04.check_one(password)
+    refute Day04.check_two(password)
+  end
+
+  test "v6" do
+    password = 111122
+
+    assert Day04.check_one(password)
+    assert Day04.check_two(password)
+  end
+
+  # test "problem" do
+  #   assert Day04.problem() == 931
+  # end
+
+
+  # test "problem2" do
+  #   assert Day04.problem2() != 481
+  # end
 end
