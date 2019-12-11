f1cac9c9f227676431dda741be4b0a1ad2c0dc1e
Tim Austin <tim@neenjaw.com>
2019-12-10 18:29:31 -0600

    part1 refactor

--- day_10/lib/asteroid_map.ex
+++ day_10/lib/asteroid_map.ex
@@ -1,5 +1,5 @@
 defmodule AsteroidMap do
-  defstruct map: nil, meta: %{}, counts: %{}
+  defstruct map: nil, meta: %{}, asteroids: []
 
   alias __MODULE__, as: AM
 
@@ -15,11 +15,10 @@ defmodule AsteroidMap do
     |> String.split("\n", trim: true)
     |> Enum.map(&String.graphemes/1)
     |> convert_to_struct()
-    |> generate_slope_mutations()
   end
 
   def convert_to_struct(map_as_lists) do
-    map =
+    list =
       map_as_lists
       |> Enum.with_index()
       |> Enum.flat_map(fn {row, y} ->
@@ -29,19 +28,17 @@ defmodule AsteroidMap do
           {{x, y}, e}
         end)
       end)
+
+    map =
+      list
       |> Enum.into(%{})
 
-    width = map_as_lists |> hd() |> length()
-    height = map_as_lists |> length()
+    asteroid_list =
+      list
+      |> Enum.filter(fn {_, o} -> asteroid?(o) end)
+      |> Enum.map(fn {c, _} -> c end)
 
-    meta =
-      %{}
-      |> put_in([:width], width)
-      |> put_in([:height], height)
-      |> put_in([:x_range], 0..(width - 1))
-      |> put_in([:y_range], 0..(height - 1))
-
-    %__MODULE__{map: map, meta: meta}
+    %__MODULE__{map: map, asteroids: asteroid_list}
   end
 
   def asteroid?(@asteroid), do: true
@@ -50,144 +47,24 @@ defmodule AsteroidMap do
   def station?(@station), do: true
   def station?(_), do: false
 
-  def asteroid?(%AM{} = asteroid_map, xy) when is_coordinate(xy) do
-    if asteroid_map.map[xy] == @asteroid, do: true, else: false
+  def generate_asteroid_visibility({x, y} = asteroid, %AM{} = asteroids) do
+    result =
+      for target <- asteroids.asteroids,
+          target != asteroid,
+          {x1, y1} = target,
+          {dx, dy} = {x - x1, y - y1},
+          angle = angle(dx, dy),
+          distance = distance(dx, dy)
+      do
+        {target, angle, distance}
+      end
+      |> Enum.group_by(fn {_, a, _} -> a end, fn {t, _, d} -> {t, d} end)
+
+    {asteroid, result}
   end
 
-  def generate_slope_mutations(%AM{meta: %{height: h, width: w}} = am) do
-    muts = [
-      {1, 1},
-      {1, -1},
-      {-1, -1},
-      {-1, 1}
-    ]
-
-    right_angle_slope = [
-      {1, 0},
-      {0, 1},
-      {-1, 0},
-      {0, -1}
-    ]
-
-    not_duplicate_slope = fn
-      1, 1 -> true
-      x, x -> false
-      _, _ -> true
-    end
-
-    slopes = for x <- 1..w, y <- 1..h, not_duplicate_slope.(x, y), do: {x, y}
-
-    # filter unreduced slopes
-    slopes =
-      slopes
-      |> Enum.filter(fn
-        {1, _} ->
-          true
-
-        {_, 1} ->
-          true
-
-        coord ->
-          not exists_reduced_slope?(slopes, coord)
-      end)
-
-    # compute quadrant slopes and add right angle slopes
-    slopes =
-      slopes
-      |> Enum.flat_map(fn {x, y} ->
-        Enum.map(muts, fn {mx, my} -> {mx * x, my * y} end)
-      end)
-      |> Kernel.++(right_angle_slope)
-
-    %{am | meta: Map.put(am.meta, :slopes, slopes)}
-  end
-
-  def exists_reduced_slope?(slopes, coord) do
-    Enum.any?(slopes, &is_reduced_slope?(coord, &1))
-  end
-
-  def is_reduced_slope?({x, y}, {a, b}) when x > a and y > b and x > y and a > b do
-    r = div(x, a)
-
-    rem(x, a) == 0 and rem(y, r) == 0
-  end
-
-  def is_reduced_slope?({x, y}, {a, b}) when x > a and y > b and y > x and b > a do
-    r = div(y, b)
-
-    rem(y, b) == 0 and rem(x, r) == 0
-  end
-
-  def is_reduced_slope?(_, _), do: false
-
-  def generate_ordered_slope_mutations(%AM{meta: %{height: h, width: w}} = am) do
-    muts = [
-      {1, 1},
-      {1, -1},
-      {-1, -1},
-      {-1, 1}
-    ]
-
-    right_angle_slope = [
-      {1, 0},
-      {0, 1},
-      {-1, 0},
-      {0, -1}
-    ]
-
-    not_duplicate_slope = fn
-      1, 1 -> true
-      x, x -> false
-      _, _ -> true
-    end
-
-    slopes = for x <- 1..w, y <- 1..h, not_duplicate_slope.(x, y), do: {x, y}
-
-    # filter unreduced slopes
-    slopes =
-      slopes
-      |> Enum.filter(fn
-        {1, _} ->
-          true
-
-        {_, 1} ->
-          true
-
-        coord ->
-          not exists_reduced_slope?(slopes, coord)
-      end)
-
-    # compute quadrant slopes and add right angle slopes
-    slopes =
-      slopes
-      |> Enum.flat_map(fn {x, y} ->
-        Enum.map(muts, fn {mx, my} -> {mx * x, my * y} end)
-      end)
-      |> Kernel.++(right_angle_slope)
-
-    # order the slopes by degrees
-    slopes =
-      slopes
-      |> Enum.map(fn c ->
-        {c, angle(c)}
-      end)
-      |> Enum.sort_by(fn {_, d} -> d end, &<=/2)
-      |> Enum.map(&elem(&1, 0))
-      # |> Enum.reverse()
-
-    # n_slopes = slopes |> length()
-
-    # slopes =
-    #   slopes
-    #   |> Stream.cycle()
-    #   |> Stream.drop_while(fn coord -> coord != {0, -1} end)
-    #   |> Enum.take(n_slopes)
-
-    %{am | meta: Map.put(am.meta, :ordered_slopes, slopes)}
-  end
-
-  def angle({x, y}) do
-    deg = (:math.atan2(y, x) * 180.0 / :math.pi()) + 90
+  def angle(dx, dy) do
+    deg = (:math.atan2(dy, dx) * 180.0 / :math.pi()) + 90
 
     deg =
       if deg < 0 do
@@ -201,5 +78,9 @@ defmodule AsteroidMap do
     else
       deg
     end
+  end
+
+  def distance(dx, dy) do
+    :math.sqrt((dx * dx) + (dy * dy))
   end
 end
--- day_10/lib/ceres_monitor.ex
+++ day_10/lib/ceres_monitor.ex
@@ -1,130 +1,21 @@
 defmodule CeresMonitor do
-  import AsteroidMap, only: [asteroid?: 1, station?: 1, is_coordinate: 1]
-
-  alias CeresMonitor.Storage
-
   def solve(map_as_string) do
     asteroid_map = AsteroidMap.parse_string_map(map_as_string)
 
-    {:ok, storage} = Storage.start(asteroid_map)
+    {:ok, {coord, m}} =
+      asteroid_map.asteroids
+      |> Task.async_stream(&AsteroidMap.generate_asteroid_visibility(&1, asteroid_map))
+      |> Enum.to_list()
+      |> Enum.max_by(fn {:ok, {_a, m}} -> map_size(m) end)
 
-    asteroid_map.map
-    |> Map.to_list()
-    |> Stream.filter(fn {_, e} -> AsteroidMap.asteroid?(e) end)
-    |> Stream.map(fn {c, _} -> c end)
-    |> Task.async_stream(&count_visible(asteroid_map, &1, storage))
-    |> Stream.run()
-
-    Storage.stop(storage).counts |> Map.to_list() |> Enum.max_by(fn {_, n} -> n end)
+    {coord, map_size(m)}
   end
 
-  def count_visible(%AsteroidMap{} = asteroid_map, coordinate, storage) do
-    %{slopes: slopes, y_range: y_range, x_range: x_range} = asteroid_map.meta
+  def solve_2(coord, map_as_string) do
+    asteroid_map = AsteroidMap.parse_string_map(map_as_string)
 
-    count =
-      slopes
-      |> Enum.reduce(0, fn slope, sum ->
-        if asteroid_along_slope?(asteroid_map.map, coordinate, slope, x_range, y_range) do
-          sum + 1
-        else
-          sum
-        end
-      end)
+    visibility = AsteroidMap.generate_asteroid_visibility(coord, asteroid_map)
 
-    Storage.put_count(storage, coordinate, count)
   end
 
-  def asteroid_along_slope?(map, coordinate, slope, x_range, y_range) do
-    coordinate
-    |> apply_slope(slope)
-    |> Stream.iterate(&apply_slope(&1, slope))
-    |> Stream.map(fn {x, y} = coordinate ->
-      cond do
-        x not in x_range -> :out_of_range
-        y not in y_range -> :out_of_range
-        map[coordinate] |> asteroid?() -> :found
-        true -> :nothing_found
-      end
-    end)
-    |> Stream.drop_while(fn
-      :nothing_found -> true
-      _ -> false
-    end)
-    |> Enum.take(1)
-    |> hd()
-    |> case do
-      :out_of_range -> false
-      :found -> true
-    end
-  end
-
-  def apply_slope({x, y}, {sx, sy}), do: {x + sx, y + sy}
-
-  def destroy_asteroids(map_as_string, coordinate) when is_coordinate(coordinate) do
-    asteroid_map =
-      map_as_string
-      |> AsteroidMap.parse_string_map()
-      |> AsteroidMap.generate_ordered_slope_mutations()
-
-    cond do
-      asteroid?(asteroid_map.map[coordinate]) -> do_destroy_asteroids(asteroid_map, coordinate)
-      station?(asteroid_map.map[coordinate]) -> do_destroy_asteroids(asteroid_map, coordinate)
-      true -> {:error, "not an asteroid to mount laser"}
-    end
-  end
-
-  def do_destroy_asteroids(asteroid_map, coordinate) do
-    {:ok, storage} = CeresMonitor.Storage.start(asteroid_map)
-
-    {:ok, coord_storage} =
-      Agent.start(fn ->
-        asteroid_map.meta.ordered_slopes
-        |> Enum.map(fn s -> {s, coordinate} end)
-        |> Enum.into(%{})
-      end)
-
-    asteroid_map.meta.ordered_slopes
-    |> Stream.cycle()
-    |> Stream.each(fn slope ->
-      last_coord = Agent.get(coord_storage, fn coords -> Map.get(coords, slope) end)
-
-      if last_coord do
-        result =
-          last_coord
-          |> apply_slope(slope)
-          |> Stream.iterate(&apply_slope(&1, slope))
-          |> Stream.map(fn {x, y} = c ->
-            {slope, c}
-            cond do
-              x not in asteroid_map.meta.x_range -> {:out_of_range, slope, c}
-              y not in asteroid_map.meta.y_range -> {:out_of_range, slope, c}
-              asteroid_map.map[c] |> asteroid?() -> {:hit, slope, c}
-              true -> :nothing_found # nothing found, keep looking along slope
-            end
-          end)
-          |> Stream.drop_while(fn
-            :nothing_found -> true
-            _ -> false
-          end)
-          |> Enum.take(1)
-          |> List.first()
-
-        case result do
-          {:out_of_range, s, _} ->
-            Agent.update(coord_storage, fn last_coord -> Map.delete(last_coord, s) end)
-
-          {:hit, s, c} ->
-            Storage.destroy_asteroid(storage, c)
-            Agent.update(coord_storage, fn last_coord -> %{last_coord | s => c} end)
-        end
-      end
-    end)
-    |> Stream.take_while(fn _ ->
-      Agent.get(coord_storage, fn last_coords -> map_size(last_coords) > 0 end)
-    end)
-    |> Stream.run()
-
-    Agent.stop(coord_storage)
-    Storage.stop(storage)
-  end
 end
